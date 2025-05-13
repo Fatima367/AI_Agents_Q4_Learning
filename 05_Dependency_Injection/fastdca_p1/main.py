db664804341ed20858e77fd52e3ffbc390ec5760
Fatima367 <princess.fatima3275@gmail.com>
2025-05-13 11:15:24 +0500

    updated

--- 05_Dependency_Injection/fastdca_p1/main.py
+++ 05_Dependency_Injection/fastdca_p1/main.py
@@ -78,7 +78,9 @@ class GetObjectOr404():
         obj = self.model.get(id)
         if not obj:
             raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Object ID {id} not found")
-        
+        return obj
+    
+    
 blog_dependency = GetObjectOr404(blogs)
 
 @app.get("/blog/{id}")
