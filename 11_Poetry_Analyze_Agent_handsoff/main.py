523aaf8ef9e5fe0fe47ac71e40f670d46b84bab2
Fatima367 <princess.fatima3275@gmail.com>
2025-07-10 20:03:46 +0500

    Handsoff Practice: Poetry Analyzer Agent

--- 11_Poetry_Analyze_Agent_handsoff/main.py
+++ 11_Poetry_Analyze_Agent_handsoff/main.py
@@ -1,6 +1,76 @@
-def main():
-    print("Hello from 11-poetry-analyze-agent-handsoff!")
+import os
+from dotenv import load_dotenv
+from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI
+from agents import enable_verbose_stdout_logging
+from agents.run import RunConfig
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+if not gemini_api_key:
+    raise ValueError("GEMINI_API_KEY not found, please check your .env file.")
+
+external_client = AsyncOpenAI(
+    api_key= gemini_api_key,
+    base_url= "https://generativelanguage.googleapis.com/v1beta/openai"
+)
+
+model = OpenAIChatCompletionsModel(
+    model= "gemini-2.0-flash",
+    openai_client= external_client
+)
+
+config = RunConfig(
+    model= model,
+    model_provider= external_client,
+    tracing_disabled= True
+)
+
+lyric_poetry_analyst = Agent(
+    name= "Lyric Poetry Analyst",
+    instructions= "You are Lyric Poetry Analyst you analyse and give a brief description (tashree) of the poetry or stanzas.",
+    handoff_description= """
+    Gives description for Lyric poetry which is when poets write about their 
+    own feelings and thoughts, like songs or poems about being sad or happy.
+    """
+)
+
+narrative_poetry_analyst = Agent(
+    name= "Narrative Poetry Analyst",
+    instructions= "You are Narrative Poetry Analyst you analyse and give a brief description (tashree) of the poetry or stanzas.",
+    handoff_description= """
+    Gives description for Narrative poetry that tells a story with characters 
+    and events, just like a regular story but written in poem form with rhymes 
+    or special rhythm.
+    """
+)
+
+dramatic_poetry_analyst = Agent(
+    name= "Dramatic Poetry Analyst",
+    instructions= "You are Dramatic Poetry Analyst you analyse and give a brief description (tashree) of the poetry or stanzas.",
+    handoff_description="""
+    Gives description for Dramatic poetry which is meant to be performed out 
+    loud, where someone acts like a character and speaks their thoughts and 
+    feelings to an audience (acting in a theatre).
+    """
+)
+
+poetry_agent = Agent(
+    name= "Poetry Analyst",
+    instructions= "You analyse the poetry type (Lyric/Narrative, Dramatic) and handsoff to the appropriate agent.",
+    handoffs= [lyric_poetry_analyst, narrative_poetry_analyst, dramatic_poetry_analyst]
+)
+
+enable_verbose_stdout_logging()
+
+result = Runner.run_sync(
+    poetry_agent, 
+    "The stars don't speak, yet still I hear, A whisper soft, when night is near.",
+    run_config= config
+    )
+
+print(f"\nAgent's Response:\n")
+print(f"{result.final_output}\n")
+print(f"Responded by: {result.last_agent.name}\n\n")
